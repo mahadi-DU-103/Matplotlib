8c93a38e45d23f45971bcee9e8d0aa5c8ac2b717
Mahadi Islam <phenomenalthe65@gmail.com>
2025-08-10 14:06:51 +0600

    Add files via upload

---
+++ Scatter plot.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('dark_background') #in case i want to use different bg colour.
+# roll = [1,2,3,4,5,6,7,8,9,10]
+# marks = [10,20,30,40,50,60,70,80,90,100]
+# #scatter plot:
+# plt.figure(figsize=(12,8)) #for a larger or smaller graph if ata is ongested.
+# plt.scatter(roll,marks,color='white',marker='x')
+# plt.plot(roll,marks,'gv',markersize = 10) #plot function can be used for any kind of plotting, g is for color and v is for marker here.
+# plt.grid(True) #gridlines
+# plt.title('student marks vs roll number') #title
+# plt.xlabel('roll number') #axis lables
+# plt.ylabel('marks')
+# plt.show()
+
+# #multiple plots on same figure
+# temp_dhaka = [30,34,33,31,36,34,33,36,32,39]
+# humidity_dhaka = [34,38,37,35,40,38,37,40,36,44]
+# temp_ctg = [34,35,36,37,28,27,26,25,31,20]
+# humidity_ctg = [40,38,36,35,42,44,41,40,34,45]
+
+# plt.figure(figsize = (7,7))
+# plt.plot(temp_dhaka,humidity_dhaka,'ro',markersize = 15)
+# plt.plot(temp_ctg,humidity_ctg,'rx',markersize = 15)
+# plt.xticks(np.arange(0,60,5)) #x axis e value gula holo x ticks.
+# plt.yticks(np.arange(0,60,5)) #y axis e value gula y ticks.
+# plt.xlabel("temperature")
+# plt.ylabel("humidity") 
+# plt.show()
+
+
+
+
+
